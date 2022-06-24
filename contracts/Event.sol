2e0d8059499da3d968b32a6ecc6be45f972537c9
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-06-24 19:56:58 +0530

    Update Event.sol

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -3,14 +3,5 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Event {
-    address public owner;
-    event Deposit(address _from, uint256 _id);
-
-    constructor() {
-        owner = msg.sender;
-    }
-
-    function deposit(uint256 _id) public {
-        emit Deposit(msg.sender, _id);
-    }
+   
 }
